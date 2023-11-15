469992b0ac066641ac873bcb8719f386c1679416
vchhajed <vchhajed@buffalo.edu>
2023-11-15 15:15:45 -0800

    added messages

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "messages" not in st.session_state:
 
 OPEN_API_MODEL = st.sidebar.selectbox(
    "OPENAI model",
-   ("gpt-3.5-turbo", "gpt-4-turbo", "gpt-4"),
+   ("gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0631", "gpt-3.5-turbo-0301", "gpt-4-1106-preview"),
    index=None,
    placeholder="Select OPENAI model",
 )
